ca7d09bb249a0ca0718527b72841d696abdc4438
Alisa Zhurova <zhurova2021@gmail.com>
2023-04-20 11:27:11 +0300

    refactor(extended-3.4): removing tag selectors

--- app.js
+++ app.js
@@ -32,19 +32,23 @@ var createNewTaskElement=function(taskString){
   var deleteButton=document.createElement("button");//delete button
   var deleteButtonImg=document.createElement("img");//delete button image
 
+  listItem.className='list-item';
+
   label.innerText=taskString;
   label.className='list-title';
 
   //Each elements, needs appending
   checkBox.type="checkbox";
+  checkBox.className='list-checkbox';
   editInput.type="text";
   editInput.className="list-task";
 
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className="edit";
+  editButton.className="list-button edit";
 
-  deleteButton.className="delete";
+  deleteButton.className="list-button delete";
   deleteButtonImg.src='./remove.svg';
+  deleteButtonImg.className="delete-icon";
   deleteButton.appendChild(deleteButtonImg);
 
 
